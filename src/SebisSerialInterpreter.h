1d3de6dcfcf35785a3eee00c9023bd1bdae830ac
SebisCodes <sebiscodes@gmail.com>
2022-12-06 07:56:55 +0100

    blockingRead has been changed to blockingBehavior
    blockingRead has been changed to blockingBehavior

--- src/SebisSerialInterpreter.h
+++ src/SebisSerialInterpreter.h
@@ -27,7 +27,7 @@ class SebisSerialInterpreter {
     void removeFunctionList(String code);
     void runFunction(String code, double value);
     void setFinalFunction(FinalFunction func);
-    void serialRead(boolean endlessLoop, boolean blockingRead);
+    void serialRead(boolean endlessLoop, boolean blockingBehavior);
 
   private:
     void interpret();
